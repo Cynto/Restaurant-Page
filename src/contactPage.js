a123828e8cccd8dd465eb41ca39832e2e0f9a408
Cynto <lucagaravello@outlook.com>
2021-03-07 19:05:57 +0100

    added contact page, fixed mobile layout

--- src/contactPage.js
+++ src/contactPage.js
@@ -2,7 +2,7 @@ import initalPageLoader from './initialPage';
 const contactLoader = (() => {
     initalPageLoader.link3.addEventListener('click', () => {
         initalPageLoader.description.querySelectorAll('*').forEach(n => n.remove());
-        initalPageLoader.description.setAttribute('style', 'width: 400px; height: 500px;')
+        initalPageLoader.description.setAttribute('style', 'width: 400px; height: 300px; opacity: 0.95')
         
         const main = document.querySelector('main');
         main.setAttribute('style', 'display: flex; background-image: url("https://wallpapercave.com/wp/wp1874162.jpg")');
@@ -16,17 +16,45 @@ const contactLoader = (() => {
         madeBy.setAttribute('style', 'margin-top: 20px; font-size: 20px; display: inline-block;')
         initalPageLoader.description.appendChild(madeBy)
 
+        
+
         const profileLink = document.createElement('a');
         profileLink.setAttribute('href', 'https://github.com/Cynto');
         profileLink.setAttribute('target', '_blank')
         profileLink.setAttribute('style', 'display: inline-block')
-        madeBy.appendChild(profileLink)
+        initalPageLoader.description.appendChild(profileLink)
+
+        const gitHub = document.createElement('h2');
+        gitHub.textContent = 'My GitHub';
+        gitHub.classList.add('gitHub');
+        gitHub.setAttribute('style', 'margin-top: 20px; font-size: 20px; display: inline-block;');
+        profileLink.appendChild(gitHub)
 
         const profile = document.createElement('img');
         profile.setAttribute('src', '/src/img/GitHub-Mark-32px.png')
         profile.classList.add('logo')
         profile.setAttribute('style', 'display: inline-block; ')
-        profileLink.appendChild(profile)
+        gitHub.appendChild(profile)
+
+        
+        const projectLink = document.createElement('a');
+        projectLink.setAttribute('href', 'https://github.com/Cynto/Restaurant-Page');
+        projectLink.setAttribute('target', '_blank');
+        initalPageLoader.description.appendChild(projectLink);
+
+        const projectGit = document.createElement('h2');
+        projectGit.textContent = 'Project Repository';
+        projectGit.classList.add('gitHub');
+        projectGit.setAttribute('style', 'margin-top: 20px; font-size: 20px; display: inline-block;');
+        projectLink.appendChild(projectGit);
+
+        const projectImg = document.createElement('img');
+        projectImg.setAttribute('src', '/src/img/GitHub-Mark-32px.png');
+        projectImg.classList.add('logo');
+        projectGit.appendChild(projectImg);
+
+
+
     })
 
 })()
